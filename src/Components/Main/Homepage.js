00615d1364d7f2e2fb300b65c08c6c0750745d53
zoleest <44841293+zoleest@users.noreply.github.com>
2023-01-17 23:16:37 +0100

    Homepage first half

--- src/Components/Main/Homepage.js
+++ src/Components/Main/Homepage.js
@@ -5,11 +5,11 @@ import David from './david.png';
 
 function Homepage() {
     return (
-        <div className="row d-flex flex-grow-1 flex-column-reverse flex-md-row">
-            <div className="col-md-5 col-lg-5 col-xl-4 d-flex align-items-end">
+        <div className="row d-flex flex-grow-1 flex-column-reverse flex-lg-row">
+            <div className="col-lg-5 col-xl-4 d-flex align-items-end">
                 <img className="img-fluid" src={David} alt="Halápi Dávid" title="Halápi Dávid" />
             </div>
-            <div className="col-12 col-md-7 col-lg-7 col-xl-8 d-block position-relative">
+            <div className="col-12 col-lg-7 col-xl-8 d-block position-relative">
                 <Bubble />
             </div>
 
